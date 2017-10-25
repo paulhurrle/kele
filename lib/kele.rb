2fdac6a4ffd68693343faf688a0f334ff3885e72
paulhurrle <paul_hurrle@yahoo.com>
2017-10-24 18:40:36 -0700

    complete initialize method which uses Bloc API to communicate with sessions endpoint and authorizes user

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,42 @@
+require 'httparty'
+require 'json'
 class Kele
 	include HTTParty
-	bloc_api 'https://www.bloc.io/api/v1'
-	user_auth_token self.class.post('https://www.bloc.io/api/v1/sessions', kele_params)
 
-	def self.initialize(bloc_api, user_auth_token)
-		@bloc_api = bloc_api
-		@user_auth_token = user_auth_token
-		@kele = Kele.new(@bloc_api, @user_auth_token)
+
+	def initialize(email, password)
+		params = { body: { email: email, password: password } }
+		#look up HTTParty documentation for base_uri
+		response = HTTParty.post(base_url("sessions"), params)
+		@token = response['auth_token']
+		@token
+	end
+
+	def me
+		response = HTTParty.get(base_url("users/me"), headers: {"authorization" => @token})
+		JSON.parse(response.body)
 	end
 
 	private
 
-    def kele_params
-        params.require(:kele).permit(:username, :password)
-    end
+	def base_url(uri)
+		"https://www.bloc.io/api/v1/#{uri}"
+	end
+
+	# bloc_api 'https://www.bloc.io/api/v1'
+	# user_auth_token self.class.post('https://www.bloc.io/api/v1/sessions', kele_params)
+
+	# def self.initialize(bloc_api, user_auth_token)
+	# 	@bloc_api = bloc_api
+	# 	@user_auth_token = user_auth_token
+	# 	@kele = Kele.new(@bloc_api, @user_auth_token)
+	# end
+
+	# private
+
+ #    def kele_params
+ #        params.require(:kele).permit(:username, :password)
+ #    end
 
 end
+# client = Kele.new(email, password)
